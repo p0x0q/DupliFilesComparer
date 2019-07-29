2392c1efc9eabf573b8a3ae714bb76eb9bc69202
p0x0q <p0x0q_jp@gmail.com>
2019-07-29 19:45:19 +0900

    release

--- DupliFilesComparer/Form1.Designer.cs
+++ DupliFilesComparer/Form1.Designer.cs
@@ -28,13 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1_files = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1_run = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3_folderpath = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1_files
+            // 
+            this.listView1_files.AllowDrop = true;
+            this.listView1_files.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1_files.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1_files.Location = new System.Drawing.Point(6, 19);
+            this.listView1_files.Name = "listView1_files";
+            this.listView1_files.Size = new System.Drawing.Size(625, 221);
+            this.listView1_files.TabIndex = 20;
+            this.listView1_files.UseCompatibleStateImageBehavior = false;
+            this.listView1_files.View = System.Windows.Forms.View.Details;
+            this.listView1_files.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListView1_files_DragDrop);
+            this.listView1_files.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListView1_files_DragEnter);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ファイル名";
+            this.columnHeader1.Width = 603;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1_files);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(637, 245);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ここにファイルをドラッグ&ドロップしてください";
+            // 
+            // button1_run
+            // 
+            this.button1_run.Location = new System.Drawing.Point(871, 234);
+            this.button1_run.Name = "button1_run";
+            this.button1_run.Size = new System.Drawing.Size(75, 23);
+            this.button1_run.TabIndex = 22;
+            this.button1_run.Text = "Run";
+            this.button1_run.UseVisualStyleBackColor = true;
+            this.button1_run.Click += new System.EventHandler(this.Button1_run_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(119, 16);
+            this.radioButton1.TabIndex = 23;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "大きいファイルを残す";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 40);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(118, 16);
+            this.radioButton2.TabIndex = 24;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "小さいファイルを残す";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(655, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 22);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "対象のフォルダパス";
+            // 
+            // textBox3_folderpath
+            // 
+            this.textBox3_folderpath.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox3_folderpath.Location = new System.Drawing.Point(655, 42);
+            this.textBox3_folderpath.Multiline = true;
+            this.textBox3_folderpath.Name = "textBox3_folderpath";
+            this.textBox3_folderpath.Size = new System.Drawing.Size(296, 77);
+            this.textBox3_folderpath.TabIndex = 25;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(790, 132);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(161, 56);
+            this.checkBox1.TabIndex = 27;
+            this.checkBox1.Text = "ファイルを上書きせずにバックアップ(bakファイル生成)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(790, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(14, 40);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 16);
+            this.radioButton3.TabIndex = 30;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Move";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(14, 18);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(49, 16);
+            this.radioButton4.TabIndex = 29;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Copy";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Location = new System.Drawing.Point(655, 194);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(101, 63);
+            this.groupBox2.TabIndex = 31;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ファイルの処理";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Location = new System.Drawing.Point(655, 125);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(129, 63);
+            this.groupBox3.TabIndex = 32;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "ファイル比較設定";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(956, 265);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3_folderpath);
+            this.Controls.Add(this.button1_run);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1_files;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1_run;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3_folderpath;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- DupliFilesComparer/Form1.cs
+++ DupliFilesComparer/Form1.cs
@@ -7,16 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections;
 
 namespace DupliFilesComparer
 {
     public partial class Form1 : Form
     {
+        public static Hashtable config = new Hashtable();
         public Form1()
         {
-            InitializeComponent();
-
-
             InitializeComponent();
             Form1.CheckForIllegalCrossThreadCalls = false;
 
@@ -25,15 +25,13 @@ namespace DupliFilesComparer
                 DLL.main.ForderCreate("./database");
             }
 
-            nao0x0.flag.debug = true;
+            //nao0x0.flag.debug = true;
 
-            def["replace_after_filename"] = textBox4_replace_after_filename.Text;
-            def["date_format"] = textBox1_date_format.Text;
+            //def["replace_after_filename"] = textBox4_replace_after_filename.Text;
+            //def["date_format"] = textBox1_date_format.Text;
 
             DLL.main.appcheck(DLL.main.myappname());
-            DLL.main.FormStart("DupliFilesRenamer", DLL.main.myappname());
-            //DLL.form.Icon("Fuji", Properties.Resources.Icon, this);
-
+            DLL.main.FormStart("DupliFilesComparer", DLL.main.myappname());
 
             string json = nao0x0.JSON.Load();
             if (json != string.Empty)
@@ -43,18 +41,6 @@ namespace DupliFilesComparer
                 if (config["folder_path"] != null)
                     textBox3_folderpath.Text = config["folder_path"].ToString();
 
-                if (config["replace_files"] != null)
-                    textBox2_replacefiles.Text = config["replace_files"].ToString();
-
-                if (config["replace_after_filename"] != null)
-                    textBox4_replace_after_filename.Text = config["replace_after_filename"].ToString();
-
-                if (config["ext"] != null)
-                    textBox1_ext.Text = config["ext"].ToString();
-
-                if (config["date_format"] != null)
-                    textBox1_date_format.Text = config["date_format"].ToString();
-
             }
 
 
@@ -63,12 +49,133 @@ namespace DupliFilesComparer
             {
                 if (cmds[1] == "/autoplay")
                 {
-                    autoplay = true;
+                    //autoplay = true;
                 }
 
             }
             catch (Exception e) { }
 
+        }
+
+        
+
+        private void Button1_run_Click(object sender, EventArgs e)
+        {
+
+            if(radioButton1.Checked != true && radioButton2.Checked != true)
+            {
+                MessageBox.Show("比較対象のいずれかにチェックを付けてください");
+                return;
+            }
+
+            button1_run.Enabled = false;
+            ArrayList file_paths = new ArrayList();
+
+            for (int i = 0; i < listView1_files.Items.Count; i++)
+            {
+                file_paths.Add(listView1_files.Items[i].Text);
+            }
+
+            string to_directory = textBox3_folderpath.Text;
+
+            int copy_mode = 0;
+            if(radioButton1.Checked == true)
+            {
+                copy_mode = 1;
+            }else if(radioButton2.Checked == true)
+            {
+                copy_mode = 2;
+            }
+
+            foreach (var value in file_paths)
+            {
+                string file_path = value.ToString();
+                string dir = Path.GetDirectoryName(file_path);
+                string name = Path.GetFileName(file_path);
+                string to_path = to_directory + @"\" + name;
+                bool flag_move = false;
+                nao0x0.PC.DebugLog(to_path);
+
+                if (File.Exists(file_path) == false)
+                    continue;
+
+                if (File.Exists(to_path) == true)
+                {
+                    nao0x0.PC.DebugLog("存在します");
+                    FileInfo fi_cur = new FileInfo(to_path);
+                    FileInfo fi_to = new FileInfo(file_path);
+                    if (copy_mode == 1 && fi_cur.Length <= fi_to.Length)
+                    {
+                        if(checkBox1.Checked == true)
+                            File.Move(to_path, to_path + ".bak");
+                        else
+                            File.Delete(to_path);
+
+                        flag_move = true;
+                    }
+                    else if (copy_mode == 2 && fi_cur.Length > fi_to.Length)
+                    {
+                        if (checkBox1.Checked == true)
+                            File.Move(to_path, to_path + ".bak");
+                        else
+                            File.Delete(to_path);
+
+                        flag_move = true;
+                    }
+                }
+                else
+                {
+                    flag_move = true;
+                    nao0x0.PC.DebugLog("存在しません");
+                }
+
+                if (flag_move == true)
+                {
+                    if (radioButton3.Checked == true)
+                        File.Move(file_path, to_path);
+                    else if (radioButton4.Checked == true)
+                        File.Copy(file_path, to_path);
+                }
+            }
+            button1_run.Enabled = true;
+        }
+
+        private void ListView1_files_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileName =
+        (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            foreach (string name in fileName)
+            {
+                listView1_files.Items.Add(name);
+            }
+        }
+
+        private void ListView1_files_DragEnter(object sender, DragEventArgs e)
+        {
+            //コントロール内にドラッグされたとき実行される
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                //ドラッグされたデータ形式を調べ、ファイルのときはコピーとする
+                e.Effect = DragDropEffects.Copy;
+            else
+                //ファイル以外は受け付けない
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Hashtable setconfig = new Hashtable();
+
+            string folder_path = textBox3_folderpath.Text;
+
+            setconfig["folder_path"] = folder_path;
+
+            nao0x0.JSON.Save(nao0x0.JSON.ToJSON(setconfig));
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            listView1_files.Items.Clear();
         }
     }
 }
